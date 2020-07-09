0b6e7951199c85f80ce956f221bb05b715530def
Michael Theos <michael@theos.me>
2020-07-09 15:57:44 +1000

    added usage and inputs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fuzzy-logic-cs6447
+
+Can be run from the cloned folder or the parent folder
+From the cloned folder:
+   Run as `python3 -m fuzzy-logic-cs6447 <binary> <input>`
+From the parent folder
+   Run as `python3 -m fuzzylogic <binary> <input>`
+
--- fuzzylogic/main.py
+++ fuzzylogic/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
+import sys
+import os
 
 def main(s):
-	print(s)
+    print(sys.argv)
+    if len(sys.argv) != 3:
+        print('Usage: <binary> <input>')
+        exit(os.EX_USAGE)
+    print(s)
+    print(f'binary: {sys.argv[1]}')
+    print(f'input: {sys.argv[2]}')
+    exit(os.EX_OK)
 
 if __name__ == '__main__':
 	main('main.py was called directly')
