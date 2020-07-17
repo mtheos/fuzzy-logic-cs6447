97b91146bc7c59c3e1e8c76e77f21ff1275f9731
Frances <francesannlee7@gmail.com>
2020-07-17 17:00:34 +1000

    added float mutator without seed

--- fuzzylogic/mutator/int_mutator.py
+++ fuzzylogic/mutator/int_mutator.py
@@ -9,13 +9,14 @@ class IntMutator :
         options = [self.add_mutation, self.minus_mutation]
         mutator = random.choice(options)
         return mutator(i)
+
     def add_mutation(self,i): # % by max int if max int is provided (??)
         i = i + random.randint(1,1000000)
         return i 
+
     def minus_mutation(self,i):
         i = i - random.randint(1,1000000)
         return i
-
 
 class FloatMutator:
     def __init__(self):
--- fuzzylogic/mutator/mutator_tester.py
+++ fuzzylogic/mutator/mutator_tester.py
@@ -1,5 +1,6 @@
 from string_mutator import *
 from int_mutator import IntMutator
+from float_mutator import *
 
 # string = "asdfghjkl"
 
@@ -10,7 +11,7 @@ from int_mutator import IntMutator
 #     print(f"{i}: {final}")
 
 num = 246
-mut = IntMutator()
+mut = FloatMutator()
 final = num
 for i in range(0,15):
     final = mut.mutate(final)
