091ddc5aba74f80d42b3e79370c75263d873d384
Frances <francesannlee7@gmail.com>
2020-07-17 18:49:28 +1000

    completed boolean mutator without seed :D

--- fuzzylogic/mutator/mutator_tester.py
+++ fuzzylogic/mutator/mutator_tester.py
@@ -1,6 +1,7 @@
 from string_mutator import *
 from int_mutator import IntMutator
 from float_mutator import *
+from boolean_mutator import *
 
 # string = "asdfghjkl"
 
@@ -10,9 +11,13 @@ from float_mutator import *
 #     final = mut.mutate(final)
 #     print(f"{i}: {final}")
 
-num = 246
-mut = FloatMutator()
-final = num
-for i in range(0,15):
-    final = mut.mutate(final)
-    print(f"{i}: {final}")
+# num = 246
+# mut = FloatMutator()
+# final = num
+# for i in range(0,15):
+#     final = mut.mutate(final)
+#     print(f"{i}: {final}")
+
+result =  False
+mut = BooleanMutator()
+print(mut.mutate(result))
