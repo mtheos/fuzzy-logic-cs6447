daafc56b0a11abb252c2f6e766749d6815b49f3c
Frances <francesannlee7@gmail.com>
2020-07-17 16:38:45 +1000

    added int_mutator without seed

--- fuzzylogic/mutator/int_mutator.py
+++ fuzzylogic/mutator/int_mutator.py
@@ -1,10 +1,18 @@
+import random
 #these all just return literally 1 thing
 #frances and aran
 class IntMutator : 
     def __init__(self):
         self.seed = 0
     def mutate(self, i):
-        self.seed += 1
+        options = [self.add_mutation, self.minus_mutation]
+        mutator = random.choice(options)
+        return mutator(i)
+    def add_mutation(self,i): # % by max int if max int is provided (??)
+        i = i + random.randint(1,1000000)
+        return i 
+    def minus_mutation(self,i):
+        i = i - random.randint(1,1000000)
         return i
 
 class FloatMutator :
@@ -39,5 +47,5 @@ class ObjectMutator :
     def __init__(self):
         self.seed = 0
     def mutate(self, i):
-        seed += 1
+        self.seed += 1
         return i
--- fuzzylogic/mutator/mutator_tester.py
+++ fuzzylogic/mutator/mutator_tester.py
@@ -1,10 +1,17 @@
 from string_mutator import *
+from int_mutator import IntMutator
 
-string = "asdfghjkl"
+# string = "asdfghjkl"
 
-mut = StringMutator()
-final = string
+# mut = StringMutator()
+# final = string
+# for i in range(0,15):
+#     final = mut.mutate(final)
+#     print(f"{i}: {final}")
+
+num = 246
+mut = IntMutator()
+final = num
 for i in range(0,15):
     final = mut.mutate(final)
     print(f"{i}: {final}")
-
