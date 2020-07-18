821b2bbd15d79131e4d60b0b3708469c90165fec
Michael Theos <michael@theos.me>
2020-07-18 13:12:53 +1000

    Use system to open processes

--- fuzzylogic/executor/runner.py
+++ fuzzylogic/executor/runner.py
@@ -1,22 +1,29 @@
 from subprocess import Popen, PIPE
 from collections import defaultdict
-# from pwn import *
+import pwn
+import os
 
 
 class Runner:
     _exit_codes_ = defaultdict(lambda: 'Nope :(')
     _exit_codes_[-6] = 'Stack Smashing! :)'
     _exit_codes_[-11] = 'Segfault! :)'
+    _exit_codes_[35584] = 'Segfault! :)'
 
     def __init__(self):
         pass
 
     def run_process(self, binary, _input):
-        return self.run_process_p_open(binary, _input)
+        return self.run_process_cmd(binary, _input)
+        # return self.run_process_p_open(binary, _input)
         # return self.run_process_pwn_tools(binary, _input)
 
+    def run_process_cmd(self, binary, _input):
+        code = os.system(f'{binary} <<EOF\n{_input}\nEOF')
+        return code
+
     def run_process_pwn_tools(self, binary, _input):
-        p = process(binary)
+        p = pwn.process(binary)
         p.send(_input)
         p.wait()
         code = p.poll()
@@ -24,7 +31,7 @@ class Runner:
 
     def run_process_p_open(self, binary, _input):
         p = Popen(binary, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = p.communicate(_input.encode())
+        p.communicate(_input.encode())
         code = p.wait()
         return code
 
