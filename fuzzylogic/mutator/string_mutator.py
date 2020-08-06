314a18d0d5a20074f75a36bbb26c0973f32a7dca
Frances <francesannlee7@gmail.com>
2020-08-06 21:45:59 +1000

    added byte flipping, format string, unicode into deterministic

--- fuzzylogic/mutator/string_mutator.py
+++ fuzzylogic/mutator/string_mutator.py
@@ -68,6 +68,28 @@ class StringMutator:
 
     def deterministic_mutator(self, i, strategy):
         #todo: do different shit depending on the strategy
-        return [self._adamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadam_generator(), 
-        "", self._insert_fmt_str_(i), i + self._adamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadam_generator()]
+        mutation_list = [self._adamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadam_generator(), 
+        "", i + self._adamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadamadam_generator()]
 
+        # byte flipping each char with the other one flipped
+        for iterator in range(len(i)):
+            temp = i 
+            changed_bit = ~(ord(temp[iterator]))
+            flipped = temp[:iterator] + (chr(changed_bit % 128)) + temp[iterator + 1:]
+            mutation_list.append(flipped)
+
+        # format string 
+        for iterator in range(len(i)):
+            temp = i
+            changed = temp[:iterator] + " %s " + temp[iterator + 1:]
+            mutation_list.append(changed)
+
+        # non ascii (unicode)
+        for iterator in range(len(i)):
+            temp = i
+            changed = temp[:iterator] + chr(random.randint(0xA1, 0xFF00)) + temp[iterator + 1:]
+            mutation_list.append(changed)
+        
+        # TODO: bitflips            
+
+        return mutation_list
