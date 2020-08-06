a30f18e1a860d154b433ce947438f627c741de7c
Michael Theos <michael@theos.me>
2020-08-06 21:38:59 +1000

    Turned jumps into ordered set, also added ability to clear jumps if memory is an issue

--- fuzzylogic/executor/traceInfo.py
+++ fuzzylogic/executor/traceInfo.py
@@ -1,18 +1,20 @@
 class TraceInfo:
     def __init__(self, jumps):
-        self._jumps = jumps
-        self._edges = {}  # edges in the execution path
-        for i in range(1, len(self._jumps)):
-            if (self._jumps[i-1], self._jumps[i]) not in self._edges:
-                self._edges[(self._jumps[i - 1], self._jumps[i])] = 1
-            else:
-                self._edges[(self._jumps[i - 1], self._jumps[i])] += 1
+        # self._jumps = jumps
+        self._jumps = sorted(set(jumps))  # sorted will make a list
+        # self._edges = {}  # edges in the execution path
+        # for i in range(1, len(self._jumps)):
+        #     if (self._jumps[i-1], self._jumps[i]) not in self._edges:
+        #         self._edges[(self._jumps[i - 1], self._jumps[i])] = 1
+        #     else:
+        #         self._edges[(self._jumps[i - 1], self._jumps[i])] += 1
 
     def jumps(self):
         return self._jumps
 
-    # def __iter__(self):
-    #     return iter(self._jumps)
+    def clear(self):
+        self._jumps = []
+        # self._edges = {}
 
     def __lt__(self, other):
         return self.priority_function() < other.priority_function()
