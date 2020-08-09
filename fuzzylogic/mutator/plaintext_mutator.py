029fabd0beb7f51f61128381d91193ce4131dcce
Frances <francesannlee7@gmail.com>
2020-08-09 23:20:31 +1000

    finished mutate function

--- fuzzylogic/mutator/plaintext_mutator.py
+++ fuzzylogic/mutator/plaintext_mutator.py
@@ -11,7 +11,38 @@ class PlainTextMutator:
         self._seed = 0
         self._original = None
         self._mutators = {str:StringMutator(), int:IntMutator(), float:FloatMutator()}
+        self.yields = None
     
+    def mutate(self, _input, strategy='none'):
+        self._analyse_(_input)
+
+        self.yields = []
+        mutated_split = []
+        crazy = []
+        for item in range(len(self._original)):
+            type_item = self._get_type_(self._original[item])
+            # print("original",self._original[item])
+            if strategy is "none":
+                mutated = self._mutators[type_item].mutate(self._original[item])
+            else:
+                mutated = self._mutators[type_item].deterministic_mutator(self._original[item], strategy)
+            crazy.append(mutated)
+            copy = list(self._original)
+            copy[item] = mutated
+            # print("copy is", copy)
+            mutated_split.append(copy)
+            # print("yield is currently", self.yields)
+        mutated_split.append(crazy)
+        for item in mutated_split:
+            # print(item)
+            string = ""
+            for items in item:
+                string += str(items)
+            self.yields.append(string)
+
+        return self.yields
+
+
     def _analyse_(self, _input):
         """
         Given plaintext input, break up the input into its respective types
@@ -34,18 +65,30 @@ class PlainTextMutator:
             return int
         elif self._is_float_(v):
             return float
+        elif self._is_str_(v):
+            return str
         raise TypeError(f'*** {v} is an unhandled type ***')
-
-    def _is_float_(self, v):
+    
+    @staticmethod
+    def _is_float_(v):
         try:
             float(v)
             return True
         except ValueError:
             return False
 
-    def _is_int_(self, v):
+    @staticmethod
+    def _is_int_(v):
         try:
             int(v)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def _is_str_(v):
+        try:
+            str(v)
             return True
         except ValueError:
             return False
