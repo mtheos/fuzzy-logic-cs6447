8aec42311c9bd6c215697c5c600b6b68e5c0e941
Frances <francesannlee7@gmail.com>
2020-08-10 16:56:40 +1000

    added plaintext mutator to fuzzer

--- fuzzylogic/mutator/__init__.py
+++ fuzzylogic/mutator/__init__.py
@@ -1,4 +1,5 @@
 from .csv_mutator import CsvMutator
 from .xml_mutator import XmlMutator
 from .json_mutator import JsonMutator
+from .plaintext_mutator import PlainTextMutator
 from .input_type_detector import detect, RET_FAIL, RET_JSON, RET_XML, RET_CSV, RET_MULTILINE_TEXT
